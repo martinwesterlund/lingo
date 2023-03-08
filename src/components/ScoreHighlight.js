bf36c17ae3331b9b3dd564537b66087e57474fd9
Martin Westerlund <martin@kodamera.se>
2023-03-08 20:36:25 +0100

    Changed text size to highlight on desktop

--- src/components/ScoreHighlight.js
+++ src/components/ScoreHighlight.js
@@ -1,5 +1,5 @@
 const ScoreHighlight = ({ score }) => {
-  return <div className="absolute top-1/2 left-1/2 text-[35vw] font-bold text-yellow-500 score-hl">+{score}</div>;
+  return <div className="absolute top-1/2 left-1/2 text-[35vw] lg:text-[15vw] font-bold text-yellow-500 score-hl">+{score}</div>;
 };
 
 export default ScoreHighlight;
