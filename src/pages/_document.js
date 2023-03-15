86650f7113a6d50a5b477a4276a45a00048fbda1
Martin Westerlund <martin@kodamera.se>
2023-03-15 19:33:27 +0100

    PWA settings update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="sv">
       <Head />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/512.png"></link>
+      <link rel="icon" href="/512.png" />
+      <meta name="theme-color" content="#212121" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
